ac3a48f0db83a35187806cb7f8363ce9aeda39a2
Jiaduobao26 <mitsuitoto@gmail.com>
2025-09-18 01:03:09 -0700

    feat(router): replace CRA demo with AppRouter and wrap app in BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,10 +9,12 @@ import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
