4a8460910ea4e8d5f40bab43f76db9c23c79dec5
Jiaduobao26 <mitsuitoto@gmail.com>
2025-09-18 02:09:07 -0700

    refactor(ui): split Header out of MainLayout

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,37 +1,14 @@
-import { Link, Outlet } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { Box } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-
+import Header from "../components/Header";
 
 export default function MainLayout() {
+
   const theme = useTheme();
   return (
     <div>
-      <AppBar position="fixed" color="transparent" elevation={0}>
-        <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            Travel Planner
-          </Typography>
-          <Button color="inherit" component={Link} to="/">
-            Home
-          </Button>
-          <Button color="inherit" component={Link} to="/itinerary">
-            ItineraryList
-          </Button>
-          <Button color="inherit" component={Link} to="/itinerary/342">
-            ItineraryDetail
-          </Button>
-          <Button color="inherit" component={Link} to="/plan">
-            Plan
-          </Button>
-          <Button color="inherit" component={Link} to="/plan/123">
-            PlanDetail
-          </Button>
-          <Button color="inherit" component={Link} to="/setup">
-            Setup
-          </Button>
-        </Toolbar>
-      </AppBar>
+      <Header />
 
       {/* Spacer using theme.mixins.toolbar to offset the AppBar */}
       <Box sx={theme.mixins.toolbar} />
