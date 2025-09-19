44098e531a4ae25330ae6a95009fca8f2a103f32
Jiaduobao26 <mitsuitoto@gmail.com>
2025-09-19 15:41:55 -0700

    chore: add temporary navigation links for development

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,11 @@
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <div>Home</div>
+  return <div>
+    HomePage
+    <Button color="inherit" component={Link} to="/setup">
+      Setup
+    </Button>
+  </div>
 }
--- src/pages/PlanList/index.js
+++ src/pages/PlanList/index.js
@@ -1,3 +1,11 @@
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+
 export default function Plan() {
-  return <div>Plan</div>
+  return <div>
+    PlanPage
+    <Button color="inherit" component={Link} to="/plan/123">
+      PlanDetail
+    </Button>
+  </div>
 }
