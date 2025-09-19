1ee66902321ed91777761fe7c143c0802e8517f4
Jiaduobao26 <mitsuitoto@gmail.com>
2025-09-19 16:08:11 -0700

    feat(header): add dynamic title, logo navigation, and trips link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { useParams, useSearchParams, useLocation, useNavigate, Link } from "react-router-dom";
+import { travelIcon } from '../assets';
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -7,20 +10,50 @@ import UserProfile from "./UserProfile";
 import { signInWithGoogle, logout } from "../lib/auth";
 import { useAuth } from "../auth/AuthContext";
 
+const TITLE = "TravelPlan";
 export default function Header() {
   const { user } = useAuth();
+
+  const { id } = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const [title, setTitle] = useState(TITLE);
+
+  useEffect(() => {
+    if (location.pathname.startsWith(`/plan/${id}`)) {
+      const destination = searchParams.get("destination");
+      destination && setTitle(`Trip to ${destination}`);
+    } else {
+      setTitle(TITLE);
+    }
+  }, [location, id, searchParams]);
+
+
   return (
     <AppBar position="fixed" color="transparent" elevation={0}>
       <Toolbar>
+        <Box
+          component="img"
+          src={travelIcon}
+          alt="Logo"
+          sx={{ width: 28, height: 28, marginRight: 1, objectFit: 'cover' }}
+          onClick={() => navigate("/")}
+        />
         <Typography variant="h3" sx={{ flexGrow: 1, fontWeight: 700 }}>
-          TravelPlan
+          {title}
         </Typography>
 
         {user ? (
-          <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-            <UserProfile uid={user.uid} />
-            <Button color="inherit" onClick={logout}>Logout</Button>
-          </Box>
+          <>
+            <Button color="inherit" component={Link} to="/plan" sx={{ mr: 8 }}>
+              Trips
+            </Button>
+            <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+              <UserProfile uid={user.uid} />
+              <Button color="inherit" onClick={logout}>Logout</Button>
+            </Box>
+          </>
         ) : (
           <Button color="inherit" onClick={signInWithGoogle}>
             Sign in
