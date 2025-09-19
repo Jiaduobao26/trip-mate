d357c5421f9b3acd200178e9923bb0aa5f9d57b4
Jiaduobao26 <mitsuitoto@gmail.com>
2025-09-18 21:08:20 -0700

    feat: update Setup page UI

--- src/pages/Setup/index.js
+++ src/pages/Setup/index.js
@@ -1,3 +1,44 @@
+import { useState } from "react";
+import { Button, Typography, Box } from "@mui/material";
+import PageContainer from "../../layouts/PageContainer";
+import PlacesAutocomplete from "../../components/PlacesAutocomplete";
 export default function Setup() {
-  return <div>Setup</div>
+  const [destination, setDestination] = useState(null);
+  const [submitting, setSubmitting] = useState(false);
+
+  const startPlanningHandler = () => {
+    setSubmitting(true);
+
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 3000);
+  }
+  return <PageContainer center sx={{ pt: 20 }}>
+    <Typography variant="h3" fontWeight={800} gutterBottom>
+      Where do you want to go?
+    </Typography>
+    <Typography color="text.secondary">
+      Pick one place. We'll take it from there.
+    </Typography>
+
+    <PlacesAutocomplete
+      sx={{ pt: 4 }}
+      size="large"
+      onSelect={(v) => setDestination(v)}
+      onClear={() => setDestination(null)}
+    />
+
+    <Box sx={{ my: 4 }}>
+      <Button
+        loading={submitting}
+        loadingPosition="start"
+        onClick={startPlanningHandler}
+        variant="contained"
+        disabled={!destination || submitting}
+      >
+        {!destination ? "Select a Place" : (submitting ? "Waiting..." : "Start planning")}
+      </Button>
+    </Box>
+
+  </PageContainer>
 }
