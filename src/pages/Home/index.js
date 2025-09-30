47e66b44d6772432422f201fa5d431a7bcd9286e
Jiaduobao26 <mitsuitoto@gmail.com>
2025-09-29 20:37:45 -0700

    feat: implement new Home page with background and sign-in/create-trip flow

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,40 @@
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
+import PageContainer from "../../layouts/PageContainer";
+import { Typography } from "@mui/material";
+import bg from "../../assets/bg.png";
+
+import { useAuth } from "../../auth/AuthContext";
+import { signInWithGoogle } from "../../lib/auth";
 
 export default function Home() {
-  return <div>
-    HomePage
-    <Button color="inherit" component={Link} to="/setup">
-      Setup
-    </Button>
-  </div>
+  const { user } = useAuth();
+  return <PageContainer
+    center
+    sx={{
+      pt: 20,
+      backgroundImage: `url(${bg})`,
+      backgroundSize: '100%',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'bottom',
+    }}
+    maxWidth="false"
+  >
+    <Typography variant="h2" fontWeight={800} gutterBottom>
+      Your journey starts here
+    </Typography>
+    <Typography color="text.secondary" sx={{ mb: 6 }}>
+      sign in now
+    </Typography>
+    {user ? < Button size="large" variant="contained" component={Link} to="/setup">
+      Create a Trip
+    </Button> :
+      < Button
+        size="large"
+        variant="contained"
+        onClick={signInWithGoogle}
+      >
+        Sign in
+      </Button>}
+  </PageContainer >
 }
